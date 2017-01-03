d905a40a2dd07c4d6b748c63958e616392496590
aroundone <1121068083@qq.com>
2017-01-03 17:53:53 +0800

    bug:解决多个事件驱动bug

--- recyclerbanner/src/main/java/com/foxmail/aroundme/recyclerbanner/GRecyclerView.java
+++ recyclerbanner/src/main/java/com/foxmail/aroundme/recyclerbanner/GRecyclerView.java
@@ -7,11 +7,13 @@ import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.View;
 
 import java.util.concurrent.TimeUnit;
 
 import rx.Observable;
 import rx.Subscriber;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 
 /**
@@ -21,6 +23,10 @@ import rx.android.schedulers.AndroidSchedulers;
 public class GRecyclerView extends RecyclerView{
 
     private int itemCount;
+
+    private boolean isCanScroll;
+    //设置全局防止重复订阅
+    private Subscription subscription;
 
     public GRecyclerView(Context context) {
         super(context);
@@ -38,13 +44,49 @@ public class GRecyclerView extends RecyclerView{
     }
 
     private void init() {
+        beginAutoScroll();
+    }
 
-        startAutoScroll();
+    private void beginAutoScroll() {
+        //取消上次订阅
+        if(subscription != null) {
+            subscription.unsubscribe();
+        }
+
+        subscription = Observable.interval(4, TimeUnit.SECONDS)
+                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<Long>() {
+            @Override
+            public void onCompleted() {
+            }
+
+            @Override
+            public void onError(Throwable e) {
+            }
+
+            @Override
+            public void onNext(Long aLong) {
+                if(isCanScroll) {
+                    smoothScrollToPosition(getVisiblePosition() + 1);
+                }
+            }
+        });
+    }
+
+    /**
+     * 设置可以自动滚动
+     */
+    public void setIsCanScroll(boolean setIsCanScroll) {
+        if(setIsCanScroll) {
+            this.isCanScroll = true;
+        } else {
+            this.isCanScroll = false;
+        }
+        beginAutoScroll();
     }
 
     /**
      * 获取当前的Position
-     * @return
+     * @return position
      */
     private int getVisiblePosition() {
         return ((LinearLayoutManager) getLayoutManager()).findLastVisibleItemPosition();
@@ -62,22 +104,28 @@ public class GRecyclerView extends RecyclerView{
     }
 
 
-    private void startAutoScroll() {
-        Observable.interval(5, TimeUnit.SECONDS)
-                .observeOn(AndroidSchedulers.mainThread()).subscribe(new Subscriber<Long>() {
-            @Override
-            public void onCompleted() {
-            }
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        setIsCanScroll(true);
+    }
 
-            @Override
-            public void onError(Throwable e) {
-            }
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        setIsCanScroll(false);
+    }
 
-            @Override
-            public void onNext(Long aLong) {
-                smoothScrollToPosition(getVisiblePosition() + 1);
-            }
-        });
+    @Override
+    protected void onWindowVisibilityChanged(int visibility) {
+        if(visibility == View.GONE){
+            // 停止轮播
+            setIsCanScroll(false);
+        }else if(visibility == View.VISIBLE){
+            // 开始轮播
+            setIsCanScroll(true);
+        }
+        super.onWindowVisibilityChanged(visibility);
     }
 
     @Override
@@ -85,5 +133,28 @@ public class GRecyclerView extends RecyclerView{
         super.smoothScrollToPosition(position);
 
         Log.d("msg", "position = " + position);
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN :
+
+                setIsCanScroll(false);
+                Log.d("msg", "ACTION_DOWN");
+                break;
+
+            case MotionEvent.ACTION_UP:
+
+                setIsCanScroll(true);
+                Log.d("msg", "ACTION_UP");
+                break;
+
+            default:
+
+                break;
+
+        }
+        return super.dispatchTouchEvent(ev);
     }
 }
