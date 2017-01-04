70d9a24cef89e52f69798b88e8c04b463c3ed721
aroundone <1121068083@qq.com>
2017-01-04 16:22:25 +0800

    change: add item click listener

--- app/src/main/java/com/foxmail/aroundme/bannertest/GAty.java
+++ app/src/main/java/com/foxmail/aroundme/bannertest/GAty.java
@@ -2,9 +2,11 @@ package com.foxmail.aroundme.bannertest;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.widget.Toast;
 
 import com.foxmail.aroundme.banner.viewpager.BannerLayout;
+import com.foxmail.aroundme.banner.viewpager.IOnItemClickListener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,8 +29,17 @@ public class GAty extends AppCompatActivity {
         urls.add("http://img0.imgtn.bdimg.com/it/u=1095909580,3513610062&fm=23&gp=0.jpg");
         urls.add("http://img4.imgtn.bdimg.com/it/u=1030604573,1579640549&fm=23&gp=0.jpg");
         urls.add("http://img5.imgtn.bdimg.com/it/u=2583054979,2860372508&fm=23&gp=0.jpg");
+        urls.add("http://img5.imgtn.bdimg.com/it/u=2583054979,2860372508&fm=23&gp=0.jpg");
+        urls.add("http://img5.imgtn.bdimg.com/it/u=2583054979,2860372508&fm=23&gp=0.jpg");
 
         bannerLayout.setUrls(urls);
+
+        bannerLayout.setOnItemClickListener(new IOnItemClickListener() {
+            @Override
+            public void OnClickListener(int position) {
+                Log.d("msg", "position = " + position);
+            }
+        });
 
     }
 }
